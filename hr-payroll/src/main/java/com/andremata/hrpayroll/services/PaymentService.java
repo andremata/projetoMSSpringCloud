1389b4b2fa5e8fe7bcb8cd3d2f137651716a7876
Andre Mata <=>
2020-12-13 20:29:10 -0300

    Feight Client

--- hr-payroll/src/main/java/com/andremata/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/andremata/hrpayroll/services/PaymentService.java
@@ -1,30 +1,19 @@
 package com.andremata.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 import com.andremata.hrpayroll.entities.Payment;
 import com.andremata.hrpayroll.entities.Worker;
+import com.andremata.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-
-	@Value("${hr-worker.host}")
-	private String WorkerHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long idWorker, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+idWorker);
-		
-		Worker worker = restTemplate.getForObject(WorkerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.find(idWorker).getBody();
 		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
