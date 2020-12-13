237c23548e074d3ac513e9b547d40b7248d041ae
Andre Mata <=>
2020-12-13 20:48:09 -0300

    Ribbon load balance

--- hr-payroll/src/main/java/com/andremata/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/andremata/hrpayroll/feignclients/WorkerFeignClient.java
@@ -1,17 +1,16 @@
 package com.andremata.hrpayroll.feignclients;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import com.andremata.hrpayroll.entities.Worker;
 
-
-
+@RibbonClient(name = "hr-worker")
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker",  path = "/workers")
 public interface WorkerFeignClient {
 	
 	@GetMapping(value = "{id}")
