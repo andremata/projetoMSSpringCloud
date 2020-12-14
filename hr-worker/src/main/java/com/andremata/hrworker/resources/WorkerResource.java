e82a5a1a1ef7de7c8134429d1b4fcff9cd73f2fb
Andre Mata <=>
2020-12-13 23:25:06 -0300

    zuul timeout

--- hr-worker/src/main/java/com/andremata/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/andremata/hrworker/resources/WorkerResource.java
@@ -36,14 +36,14 @@ public class WorkerResource {
 	@GetMapping(value = "{id}")
 	public ResponseEntity<Worker> find(@PathVariable Long id) {
 		
-		/*
-		try {
+		
+		/*try {
 			Thread.sleep(3000L);
 			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}
-		*/
+		}*/
+		
 		
 		logger.info("PORT: " + env.getProperty("local.server.port"));
 		
