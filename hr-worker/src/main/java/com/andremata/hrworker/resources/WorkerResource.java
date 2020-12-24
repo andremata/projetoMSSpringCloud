a93e1197feef3905b5846e6656a0e2069b07327a
Andre Mata <=>
2020-12-23 23:18:31 -0300

    config server client hr-worker

--- hr-worker/src/main/java/com/andremata/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/andremata/hrworker/resources/WorkerResource.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.andremata.hrworker.entities.Worker;
 import com.andremata.hrworker.repositories.WorkerRepository;
 
@@ -20,11 +20,21 @@ public class WorkerResource {
 
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	Environment env;
 	
 	@Autowired
 	private WorkerRepository repository;
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig);
+		
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
