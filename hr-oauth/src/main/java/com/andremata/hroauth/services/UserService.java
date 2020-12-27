94b966218f9323e234393b8fa54392827a8f9f53
Andre Mata <=>
2020-12-26 21:03:47 -0300

    login jwt token

--- hr-oauth/src/main/java/com/andremata/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/andremata/hroauth/services/UserService.java
@@ -2,13 +2,15 @@ package com.andremata.hroauth.services;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import com.andremata.hroauth.entities.User;
 import com.andremata.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 
@@ -24,6 +26,20 @@ public class UserService {
 		}
 		
 		logger.info("Email encontrado " + email);
+		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = feignClient.findByEmail(username).getBody();
+		
+		if (user == null) {
+			logger.error("Email não encontrado " + username);
+			throw new UsernameNotFoundException("Email não encontrado!");
+		}
+		
+		logger.info("Email encontrado " + username);
 		
 		return user;
 	}
